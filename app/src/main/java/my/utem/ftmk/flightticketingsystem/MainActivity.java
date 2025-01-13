43cdeca5e2cf9cfad1d9d1e0f7302c09c542bb2c
soonkit-king <kitking2002@gmail.com>
2025-01-13 23:37:52 +0800

    menu_bottom_nav stuff and it's fragments

--- app/src/main/java/my/utem/ftmk/flightticketingsystem/MainActivity.java
+++ app/src/main/java/my/utem/ftmk/flightticketingsystem/MainActivity.java
@@ -7,18 +7,40 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        // Set default fragment
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.nav_host_fragment, new BookFlight())
+                .commit();
+
+        // Handle navigation item clicks
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+
+            if (item.getItemId() == R.id.nav_book_flight) {
+                selectedFragment = new BookFlight();
+            } else if (item.getItemId() == R.id.nav_booking_history) {
+                selectedFragment = new BookingHistory();
+            }
+
+            if (selectedFragment != null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.nav_host_fragment, selectedFragment)
+                        .commit();
+            }
+            return true;
         });
     }
 }
