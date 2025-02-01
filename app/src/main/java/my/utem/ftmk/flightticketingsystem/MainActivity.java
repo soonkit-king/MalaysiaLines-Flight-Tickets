5aad847cba9f1b37dfa5935799533acce9e34144
soonkit-king <kitking2002@gmail.com>
2025-02-02 00:32:33 +0800

    t5g6yu7i

--- app/src/main/java/my/utem/ftmk/flightticketingsystem/MainActivity.java
+++ app/src/main/java/my/utem/ftmk/flightticketingsystem/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);uuu
         setContentView(R.layout.activity_main);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
