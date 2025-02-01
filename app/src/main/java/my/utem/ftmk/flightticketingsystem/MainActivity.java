6de07d8b4097a6952fd79992edf09ac13aeacc1e
soonkit-king <kitking2002@gmail.com>
2025-02-02 00:44:07 +0800

    drftgyhj

--- app/src/main/java/my/utem/ftmk/flightticketingsystem/MainActivity.java
+++ app/src/main/java/my/utem/ftmk/flightticketingsystem/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.nav_host_fragment, new BookFlight())
                 .commit();
-dertfyguh
+dertfyguhtfgyhbgvftgyh
         // Handle navigation item clicks
         bottomNavigationView.setOnItemSelectedListener(item -> {
             Fragment selectedFragment = null;
