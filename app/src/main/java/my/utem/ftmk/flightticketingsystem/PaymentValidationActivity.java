e31a85a04c0b7538767554742186d6c991083412
Zulhelmi <101618544+zulbiasa@users.noreply.github.com>
2025-02-14 00:19:17 +0800

    Fix seatNo into String

--- app/src/main/java/my/utem/ftmk/flightticketingsystem/PaymentValidationActivity.java
+++ app/src/main/java/my/utem/ftmk/flightticketingsystem/PaymentValidationActivity.java
@@ -127,6 +127,7 @@ public class PaymentValidationActivity extends AppCompatActivity {
         String departureDatetime = sharedPreferences.getString(PrefKey.KEY_DEPARTURE_DATETIME, "");
         String arrivalDatetime = sharedPreferences.getString(PrefKey.KEY_ARRIVAL_DATETIME, "");
         Set<String> seatNo = sharedPreferences.getStringSet(PrefKey.KEY_SELECTED_SEATS, new HashSet<>());
+        String seatNoString = TextUtils.join(",", seatNo);
         boolean refund = sharedPreferences.getBoolean(PrefKey.KEY_REFUND_GUARANTEE, false);
         double totalPayment = sharedPreferences.getFloat(PrefKey.KEY_TOTAL_PAYMENT, -1);
 
@@ -152,7 +153,7 @@ public class PaymentValidationActivity extends AppCompatActivity {
             pax,
             departureDatetime,
             arrivalDatetime,
-            seatNo,
+            seatNoString,
             refund,
             totalPayment,
             // Contact details
